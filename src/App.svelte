9bd986910b9e4f7ff1281f237b6bf70756dc473f
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2022-09-01 18:38:52 -0300

    ✨ NEW FEATURE: binomial formula

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,32 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import "./app.css";
+  import { Router, Link, Route } from "svelte-routing";
+  import BinomialFormula from "./pages/BinomialFormula.svelte";
+  // import ComplexNumbers from "./pages/ComplexNumbers.svelte";
+
+  export let url = ""
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<Router { url }>
+  <nav>
+    <Link to="/binomial-formula">Fórmula Binomial</Link>
+    <!-- <Link to="/complex-numbers">Números Complexos</Link> -->
+  </nav>
+  <main>
+    <Route path="/binomial-formula" component={ BinomialFormula } />
+    <!-- <Route path="/complex-numbers" component={ ComplexNumbers } /> -->
+  </main>
+</Router>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  nav {
+    display: flex;
+    gap: 2rem;
+    padding: 2rem;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  main {
+    margin-top: 4rem;
+    text-align: center;
   }
 </style>
